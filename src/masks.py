13292139cdeb139d41793b59749829816d857c7d
Evgeniy S <abc9910099gmail.com>
2026-09-01 21:33:20 +0400

    add new function

--- src/masks.py
+++ src/masks.py
@@ -10,10 +10,7 @@ def get_mask_card_number(num_card: str) -> str:
     for i in range(0, len(source_string), 4):
         new_list_nums.append(source_string[i : i + 4])
     return " ".join(new_list_nums)
-
-
-print(get_mask_card_number("9994567891234552"))
-
+print(get_mask_card_number("1234567891234567"))
 
 def get_mask_account(bank_account: str) -> str:
     """Функция которая маскирует номер счета пользователя"""
@@ -28,5 +25,4 @@ def get_mask_account(bank_account: str) -> str:
 
     return bank_account_mask
 
-
-print(get_mask_account("459446512123"))
+print(get_mask_account("123456789123456789123456789"))
