f099631d3a81fcd1a02486fcdc11905f8b810e4b
Evgeniy S <abc9910099gmail.com>
2026-09-08 17:18:06 +0400

    add new function sort_by_date into processing

--- src/processing.py
+++ src/processing.py
@@ -7,6 +7,11 @@ def filter_by_state(list_of_dict: list, key_state='EXECUTED') -> list:
             result_list.append(i)
     return result_list
 
+def sort_by_date(list_of_dict: list, sort_order=True) -> list:
+    '''Функция принимает список словарей и необязательный параметр, задающий порядок
+    сортировки (по умолчанию — убывание) и возвращает новый список, отсортированный по дате'''
+    sorted_list_dict = sorted(list_of_dict, key=lambda x: x['date'], reverse=sort_order)
+    return sorted_list_dict
 
 
 
@@ -14,3 +19,8 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True))
